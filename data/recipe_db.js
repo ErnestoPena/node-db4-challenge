05141d33859c357394a1712ce80bcc6ac0edb39a
Ernesto Pena <ernestopenad@gmail.com>
2019-08-18 19:57:42 -0500

    GET for all recipies and the stretch function that returns a list of ingredients and quantites for a single recipe at /id/shoppinglist has been implemented

---
+++ data/recipe_db.js
@@ -0,0 +1,17 @@
+const db = require('../data/db-config');
+
+module.exports = {
+    find,
+    findInstructionsByID
+}
+
+function find() {
+    return db('recipe');
+}
+
+function findInstructionsByID(id) {
+    return db.select('IBR.recipe_id' , 'I.name' , 'IBR.quantities')
+             .from('ingredientsbyrecipe as IBR')
+             .join('ingredients as I' , 'IBR.id' , 'I.id')
+             .join('units as U' , 'I.unit_id' , 'U.id').where('IBR.recipe_id', '=' , id);
+}
--- server.js
+++ server.js
@@ -4,6 +4,12 @@ const server = express();
 const port = 5000;
 server.use(express.json());
 
+const recipeRouter = require('./api/recipe-router');
+const ingredientRouter = require('./api/ingredient-router');
+
+server.use('/api/recipe' , recipeRouter);
+server.use('/api/ingredient', ingredientRouter);
+
 
 server.get('/', (req,res) => {
     res.send('This is the recipe API')
@@ -14,3 +20,6 @@ server.get('/', (req,res) => {
 server.listen( port , (req,res) => {
     console.log(`Server is listening on port ${port}`)
 })
+
+
+module.export = server;
